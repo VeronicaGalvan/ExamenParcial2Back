b43d7163cd6c4adae5f8205ec178d5354bb5e04b
VeronicaGalvan <lizethegalvan@gmail.com>
2025-11-08 00:08:55 -0600

    Add project files

--- Examen_parcial2back/obj/Debug/net9.0/Examen_parcial2back.AssemblyInfo.cs
+++ Examen_parcial2back/obj/Debug/net9.0/Examen_parcial2back.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examen_parcial2back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6280d8b4f9684245e5d0a44ef9cf966f1ffdc385")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examen_parcial2back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examen_parcial2back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
